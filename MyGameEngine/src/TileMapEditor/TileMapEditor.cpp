7f9d05824e48b216ae32b802a5592c649a86ec82
Alan <alan_pires2@hotmail.com>
2024-11-06 19:09:23 -0300

    tilemapeditor

--- MyGameEngine/src/TileMapEditor/TileMapEditor.cpp
+++ MyGameEngine/src/TileMapEditor/TileMapEditor.cpp
@@ -54,11 +54,23 @@ void TileMapEditor::Initialize()
 		Logger::Err("Error creating SDL Renderer");
 		return;
 	}
+	// Initialize SDL2_image for JPG support
+	if (!(IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG))
+	{
+		Logger::Err("Error in IMG_Init");
+		return;
+	}
 	isRunning = true;
+}
+
+void TileMapEditor::Setup()
+{
+
 }
 
 void TileMapEditor::Run()
 {
+	Setup();
 	while (isRunning)
 	{
 		ProcessInput();
@@ -72,24 +84,32 @@ void TileMapEditor::ProcessInput()
 	SDL_Event sdlEvent;
 	const Uint8* keys = SDL_GetKeyboardState(NULL);
 
+	mouseState = SDL_GetMouseState(&mouseX, &mouseY);
+
 	while (SDL_PollEvent(&sdlEvent))
 	{
 		if (sdlEvent.type == SDL_QUIT)
 			isRunning = false;
 		if (keys[SDL_SCANCODE_ESCAPE])
 			isRunning = false;
+		if (sdlEvent.type == SDL_MOUSEBUTTONDOWN)
+		{
+			if (sdlEvent.button.button == SDL_BUTTON_LEFT)
+				PrintXY();
+		}
 	}
-	mouseState = SDL_GetMouseState(&mouseX, &mouseY);
+}
+
+void TileMapEditor::PrintXY()
+{
+	Logger::Log("X = " + std::to_string(mouseX));
+	Logger::Log("Y = " + std::to_string(mouseY));
+	SDL_Delay(100);
 }
 
 void TileMapEditor::Update()
 {
-	if (mouseState & SDL_BUTTON(SDL_BUTTON_LEFT))
-	{
-		Logger::Log("X = " + std::to_string(mouseX));
-		Logger::Log("Y = " + std::to_string(mouseY));
-		SDL_Delay(100);
-	}
+
 }
 
 void TileMapEditor::Render()
@@ -97,7 +117,43 @@ void TileMapEditor::Render()
 	SDL_SetRenderDrawColor(renderer, 21, 21, 21, 255);
 	SDL_RenderClear(renderer);
 
+	RenderTileMap();
+
 	SDL_RenderPresent(renderer);
+}
+
+SDL_Texture* TileMapEditor::LoadTexture(const char* filePath, SDL_Renderer* renderer)
+{
+	SDL_Texture* texture = nullptr;
+	SDL_Surface* surface = IMG_Load(filePath);
+	if (!surface)
+	{
+		Logger::Err("Error loading surface: ");
+		return nullptr;
+	}
+	texture = SDL_CreateTextureFromSurface(renderer, surface);
+	if (!texture)
+	{
+		Logger::Err("Error creating Texture from Surface");
+		return nullptr;
+	}
+	//SDL_FreeSurface(surface);
+	return texture;
+}
+
+void TileMapEditor::RenderTileMap()
+{
+	//SDL_Rect rect = {
+	//	0, 800, 400, 240
+	//};
+	SDL_Texture* jpgTexture = LoadTexture("./assets/tilemaps/jungle.png", renderer);
+	if (!jpgTexture)
+	{
+		Logger::Err("Error loading texture");
+		return;
+	}
+	SDL_RenderCopy(renderer, jpgTexture, NULL, NULL);
+
 }
 
 void TileMapEditor::Destroy()
